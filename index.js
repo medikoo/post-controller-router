e3f07af91a5e483e17c3c552ffe311159add11e9
Mariusz Nowak <medyk@medikoo.com>
2015-06-24 14:56:47 +0200

    Integrate validation and normalization into class

--- index.js
+++ index.js
@@ -2,27 +2,150 @@
 
 'use strict';
 
-var mixin            = require('es5-ext/object/mixin-prototypes')
+var customError      = require('es5-ext/error/custom')
+  , ensureCallable   = require('es5-ext/object/valid-callable')
+  , ensureObject     = require('es5-ext/object/valid-object')
+  , forEach          = require('es5-ext/object/for-each')
+  , isObject         = require('es5-ext/object/is-object')
+  , mixin            = require('es5-ext/object/mixin-prototypes')
   , setPrototypeOf   = require('es5-ext/object/set-prototype-of')
   , d                = require('d')
+  , isPromise        = require('is-promise')
   , ControllerRouter = require('controller-router')
-  , normalize        = require('./lib/normalize')
+  , isStatic         = require('controller-router/lib/is-token-static')
 
-  , defineProperty = Object.defineProperty;
+  , push = Array.prototype.push, create = Object.create, stringify = JSON.stringify;
 
-var PostControllerRouter = module.exports = function (routes/*, options*/) {
-	var options;
+var PostControllerRouter = module.exports = Object.defineProperties(function (routes/*, options*/) {
 	if (!(this instanceof PostControllerRouter)) {
 		return new PostControllerRouter(routes, arguments[1]);
 	}
-	options = Object(arguments[1]);
-	ControllerRouter.call(this, normalize(routes, options), options);
-	defineProperty(this, 'routes', d(routes));
-};
+	ControllerRouter.call(this, routes, arguments[1]);
+}, {
+	ensureRoutes: d(function (routes/*, options*/) {
+		var options = Object(arguments[1]), defValidate, defSubmit, defRemoteSubmit;
+
+		ensureObject(routes);
+		defValidate = (options.validate == null) ? null : ensureCallable(options.validate);
+		defSubmit = (options.submit == null) ? null : ensureCallable(options.submit);
+		defRemoteSubmit = (options.remoteSubmit == null) ? null : ensureCallable(options.remoteSubmit);
+
+		forEach(routes, function (conf, path) {
+			var isDynamic = (path === '/') ? false : !path.split('/').every(isStatic);
+			if (conf === true) {
+				conf = {};
+			} else if (!isObject(conf)) {
+				throw customError("Invalid configuration for " + stringify(path), 'INVALID_CONFIGURATION');
+			}
+			if (isDynamic) {
+				if (typeof conf.match !== 'function') {
+					throw customError("Missing match function for " + stringify(path), 'MISSING_MATCH');
+				}
+			}
+			if (conf.validate == null) {
+				if (!defValidate) {
+					throw customError("Missing validate function for " + stringify(path), 'MISSING_VALIDATE');
+				}
+			} else {
+				if (typeof conf.validate !== 'function') {
+					throw customError("Invalid validate function for " + stringify(path), 'INVALID_VALIDATE');
+				}
+			}
+			if (conf.submit == null) {
+				if (conf.remoteSubmit != null) {
+					if (conf.remoteSubmit === true) {
+						if (!defRemoteSubmit) {
+							throw customError("Missing remoteSubmit function for " + stringify(path),
+								'MISSING_REMOTE_SUBMIT');
+						}
+					} else {
+						if (typeof conf.remoteSubmit !== 'function') {
+							throw customError("Invalid remote submit function for " + stringify(path),
+								'INVALID_REMOTE_SUBMIT');
+						}
+					}
+					if (conf.processResponse) {
+						if (typeof conf.processResponse !== 'function') {
+							throw customError("Invalid process response function for " + stringify(path),
+								'INVALID_PROCESS_RESPONSE');
+						}
+					}
+				} else {
+					if (!defSubmit) {
+						throw customError("Missing submit function for " + stringify(path), 'MISSING_SUBMIT');
+					}
+				}
+			} else {
+				if (typeof conf.submit !== 'function') {
+					throw customError("Invalid submit function for " + stringify(path), 'INVALID_SUBMIT');
+				}
+			}
+		});
+		return routes;
+	}),
+	resolveRoutes: d(function (routes/*, options*/) {
+		var options = Object(arguments[1]), resolvedRoutes = create(null);
+
+		forEach(routes, function (conf, path) {
+			var resolvedConf, remoteSubmit, processResponse;
+			if (conf === true) {
+				resolvedRoutes[path] = {
+					validate: options.validate,
+					submit: options.submit
+				};
+				return;
+			}
+			resolvedConf = resolvedRoutes[path] = {};
+			if (conf.match) resolvedConf.match = conf.match;
+			resolvedConf.validate = conf.validate || options.validate;
+			if (conf.submit) {
+				resolvedConf.submit = conf.submit;
+			} else if (conf.remoteSubmit) {
+				remoteSubmit = (conf.remoteSubmit === true) ? options.remoteSubmit : conf.remoteSubmit;
+				if (conf.processResponse) {
+					processResponse = conf.processResponse;
+					resolvedConf.submit = function () {
+						var result = remoteSubmit.apply(this, arguments);
+						if (isPromise(result)) return result.then(processResponse.bind(this));
+						return processResponse.call(this, result);
+					};
+				} else {
+					resolvedConf.submit = remoteSubmit;
+				}
+			} else {
+				resolvedConf.submit = options.submit;
+			}
+		});
+		return resolvedRoutes;
+	}),
+	normalizeRoutes: d(function (routes/*, options*/) {
+		var normalizedRoutes = create(null);
+
+		forEach(this.resolveRoutes(routes, arguments[1]), function (conf, path) {
+			var normalizedConf, validate = conf.validate, submit = conf.submit;
+			normalizedConf = normalizedRoutes[path] = {};
+			if (conf.match) normalizedConf.match = conf.match;
+			normalizedConf.controller = function () {
+				var result = validate.apply(this, arguments), inputArgs = arguments, args;
+				if (isPromise(result)) {
+					return result.then(function (result) {
+						var args = [result];
+						push.apply(args, inputArgs);
+						return submit.apply(this, args);
+					});
+				}
+				args = [result];
+				push.apply(args, inputArgs);
+				return submit.apply(this, args);
+			};
+		});
+		return normalizedRoutes;
+	})
+});
 
 if (setPrototypeOf) setPrototypeOf(PostControllerRouter, ControllerRouter);
 else mixin(PostControllerRouter, ControllerRouter);
 
 PostControllerRouter.prototype = Object.create(ControllerRouter.prototype, {
-	controller: d(PostControllerRouter)
+	constructor: d(PostControllerRouter)
 });
--- test/index.js
+++ test/index.js
@@ -2,52 +2,109 @@
 
 var clear = require('es5-ext/array/#/clear');
 
-module.exports = function (T, a) {
-	var conf = {}, router, called = [], event = {};
-	conf = {
-		foo: {
-			validate: function () { called.push('foo:validate'); },
-			submit: function () { called.push('foo:submit'); },
-			returnUrl: 'marko'
-		},
-		'elo/[a-z]+': {
-			match: function (a1) { called.push('elo:match'); return true; },
-			validate: function () { called.push('elo:validate'); },
-			submit: function () { called.push('elo:submit'); }
-		},
-		miszka: true,
-		remote: {
-			remoteSubmit: function (x) {
-				called.push('remote:remoteSubmit');
+module.exports = {
+	Ensure: function (T, a) {
+		var conf = {}, result, called = [], opts;
+		conf = {
+			foo: {
+				validate: function () { called.push('foo:validate'); },
+				submit: function () { called.push('foo:submit'); },
+				redirectUrl: 'marko'
+			},
+			'elo/[a-z]+': {
+				match: function (a1) { called.push('elo:match'); },
+				validate: function () { called.push('elo:validate'); },
+				submit: function () { called.push('elo:submit'); }
+			}
+		};
+		result = T.normalizeRoutes(T.ensureRoutes(conf));
+		a.not(result, conf);
+		result.foo.controller();
+		a.deep(called, ['foo:validate', 'foo:submit']);
+		clear.call(called);
+
+		a.deep(result['elo/[a-z]+'], { match: conf['elo/[a-z]+'].match,
+			controller: result['elo/[a-z]+'].controller });
+		result['elo/[a-z]+'].controller();
+		a.deep(called, ['elo:validate', 'elo:submit']);
+		clear.call(called);
+
+		conf.bla = true;
+		a.throws(function () { T.ensureRoutes(conf); }, 'MISSING_VALIDATE');
+		delete conf.bla;
+
+		delete conf.foo.validate;
+		a.throws(function () { T.ensureRoutes(conf); }, 'MISSING_VALIDATE');
+
+		opts = { validate: function () {} };
+		T.ensureRoutes(conf, opts);
+
+		conf.bla = true;
+		a.throws(function () { T.ensureRoutes(conf, opts); }, 'MISSING_SUBMIT');
+		delete conf.bla;
+
+		delete conf.foo.submit;
+		a.throws(function () { T.ensureRoutes(conf, opts); }, 'MISSING_SUBMIT');
+
+		opts.submit = function () {};
+		T.ensureRoutes(conf, opts);
+
+		conf.bla = true;
+		T.ensureRoutes(conf, opts);
+
+		conf.foo.remoteSubmit = true;
+		a.throws(function () { T.ensureRoutes(conf, opts); }, 'MISSING_REMOTE_SUBMIT');
+
+		opts.remoteSubmit = function () {};
+		T.ensureRoutes(conf, opts);
+	},
+	Router: function (T, a) {
+		var conf = {}, router, called = [], event = {};
+		conf = {
+			foo: {
+				validate: function () { called.push('foo:validate'); },
+				submit: function () { called.push('foo:submit'); },
+				returnUrl: 'marko'
+			},
+			'elo/[a-z]+': {
+				match: function (a1) { called.push('elo:match'); return true; },
+				validate: function () { called.push('elo:validate'); },
+				submit: function () { called.push('elo:submit'); }
+			},
+			miszka: true,
+			remote: {
+				remoteSubmit: function (x) {
+					called.push('remote:remoteSubmit');
+					return x;
+				},
+				processResponse: function (x) {
+					called.push('remote:processResponse');
+					return x;
+				}
+			}
+		};
+		router = new T(conf, {
+			validate: function (x) {
+				called.push('validate');
 				return x;
 			},
-			processResponse: function (x) {
-				called.push('remote:processResponse');
-				return x;
+			submit: function () {
+				called.push('submit');
 			}
-		}
-	};
-	router = new T(conf, {
-		validate: function (x) {
-			called.push('validate');
-			return x;
-		},
-		submit: function () {
-			called.push('submit');
-		}
-	});
-	a.deep(router.routeEvent(event, 'foo'),
-		{ conf: conf.foo, result: undefined, event: event });
-	a.deep(called, ['foo:validate', 'foo:submit']);
-	clear.call(called);
+		});
+		a.deep(router.routeEvent(event, 'foo'),
+			{ conf: conf.foo, result: undefined, event: event });
+		a.deep(called, ['foo:validate', 'foo:submit']);
+		clear.call(called);
 
-	a.deep(router.routeEvent(event, 'elo/fiszka'),
-		{ conf: conf['elo/[a-z]+'], result: undefined, event: event });
-	a.deep(called, ['elo:match', 'elo:validate', 'elo:submit']);
-	clear.call(called);
+		a.deep(router.routeEvent(event, 'elo/fiszka'),
+			{ conf: conf['elo/[a-z]+'], result: undefined, event: event });
+		a.deep(called, ['elo:match', 'elo:validate', 'elo:submit']);
+		clear.call(called);
 
-	a.deep(router.routeEvent(event, 'remote'),
-		{ conf: conf.remote, result: undefined, event: event });
-	a.deep(called, ['validate', 'remote:remoteSubmit', 'remote:processResponse']);
-	clear.call(called);
+		a.deep(router.routeEvent(event, 'remote'),
+			{ conf: conf.remote, result: undefined, event: event });
+		a.deep(called, ['validate', 'remote:remoteSubmit', 'remote:processResponse']);
+		clear.call(called);
+	}
 };
